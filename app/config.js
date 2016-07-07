6cf148dabc7fa9e6409ed7fce66f1cad79c14739
Hack Reactor Students <accounts@hackreactor.com>
2016-07-06 19:25:04 -0700

    Convert user model to MongoDB

--- app/config.js
+++ app/config.js
@@ -4,8 +4,3 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/shortly');
 
 module.exports = mongoose;
-
-module.exports.User = mongoose.model('User', {
-  username: { type: String, unique: true },
-  password: String
-});
--- app/models/user.js
+++ app/models/user.js
@@ -2,24 +2,42 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function() {
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function() {
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var cipher = Promise.promisify(bcrypt.hash);
+
+var User = db.model('User', {
+  username: { type: String, unique: true },
+  password: String
 });
 
-module.exports = User;
+
+module.exports = {
+  create: function(username, password) {
+    return this.hashPassword(password).then(function(hash) {
+      return new Promise(function(resolve, reject) {
+        var user = new User({username: username, password: hash});
+        user.save(function(err) {
+          err ? reject(err) : resolve(user);
+        });
+      });
+    });
+  },
+  validateUser: function(username, password) {
+    var user;
+    return new Promise(function(resolve, reject) {
+      User.findOne({ username: username }, 'username password', function(err, user) {
+        err ? reject(err) : resolve(user);
+      });
+    })
+    .then(function(result) {
+      user = result;
+      return new Promise(function(resolve, reject) {
+        bcrypt.compare(password, user.password, function(err, result) {
+          err ? reject(err) : resolve(result);          
+        });
+      });
+    });
+  }, 
+  hashPassword: function(password) {
+    return cipher(password, null, null);
+  }
+};
