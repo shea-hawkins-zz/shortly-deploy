6fe7eca4247d54a82e830c6e1474b291ceeb2b35
Hack Reactor Students <accounts@hackreactor.com>
2016-07-06 17:32:10 -0700

    Use mongo for link creation

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,42 @@
 var db = require('../config');
 var crypto = require('crypto');
+var util = require('../../lib/utility');
+// Link methods
+// createLink(url) // return a Link that's been added to DB
+// clickLink(Link) // increment visits on link
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function() {
-    this.on('creating', function(model, attrs, options) {
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var Link = db.model('Link', {
+  url: { type: String, unique: true },
+  baseUrl: String,
+  code: String,
+  title: String,
+  visits: String
 });
 
-module.exports = Link;
+module.exports = {
+  create: function(url) {
+    var shasum = crypto.createHash('sha1');
+    shasum.update(url);
+    var code = shasum.digest('hex').slice(0, 5);
+    return new Promise(function(resolve, reject) {
+      util.getUrlTitle(url, function(err, title) {
+        err ? reject(err) : resolve(title);
+      });
+    })
+    .then(function(title) {
+      return new Promise(function(resolve, reject) {
+        var link = new Link({url: url, code: code, visits: 0, title: title});
+        link.save(function(err) {
+          err ? reject(err) : resolve(link);
+        });
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  }
+};
+
+module.exports.create('http://facebook.com').then(function(link) {
+  console.log(link);
+});
